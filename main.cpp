ded3da2c49a8741065a7906553482c596f5dd5f5
Martinmoudime <martinjuju025@gmail.com>
2024-10-27 16:56:50 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,14 @@
 const int taille1 = 2;
 const int taille2 = 3;
 
+float T[taille1][taille1] ={};
+float T1[taille1][taille1] ={};
+float T2[taille1][taille1] ={};
+
+float T_33[taille2][taille2] ={};
+float T1_33[taille2][taille2] ={};
+float T2_33[taille2][taille2] ={};
+
 float T_FINAL22[taille1][taille1] = {};
 float T_FINAL33[taille2][taille2] = {};
 
@@ -31,30 +39,35 @@ int main (int argc, char** argv) {
                 std::cin >> choix2;
                 switch(choix2) {
                     case 1:
-                        Somme22(T_FINAL22);
+                        lecture2(T1, T2);
+                        Somme22(T_FINAL22, T1, T2);
                         std::cout << "La somme est  :\n";
                         std::cout << "( "  << T_FINAL22[0][0] << " " <<  T_FINAL22[0][1] << " )\n";
                          std::cout << "( "  << T_FINAL22[1][0] << " " <<  T_FINAL22[1][1] << " )\n";
                     break;
                     case 2:
-                        Soustracion22(T_FINAL22);
+                        lecture2(T1, T2);
+                        Soustracion22(T_FINAL22, T1, T2);
                         std::cout << "La soustraction est :\n";
                         std::cout << "( "  << T_FINAL22[0][0] << " " <<  T_FINAL22[0][1] << " )\n";
                         std::cout << "( "  << T_FINAL22[1][0] << " " <<  T_FINAL22[1][1] << " )\n";
                     break;
                     case 3:
-                        Multiplication22(T_FINAL22);
+                        lecture2(T1, T2);
+                        Multiplication22(T_FINAL22, T1, T2);
                         std::cout << "Le produit est :\n";
                         std::cout << "( "  << T_FINAL22[0][0] << " " <<  T_FINAL22[0][1] << " )\n";
                         std::cout << "( "  << T_FINAL22[1][0] << " " <<  T_FINAL22[1][1] << " )\n";
                     break;
                     case 4:
-                        Determinant22(det);
+                        lecture1(T);
+                        Determinant22(det, T);
                         std::cout << "Le determinant est :" << det << "\n";
                     break;
                     case 5:
+                        lecture1(T);
                         temoin = 0;
-                        Inverse22(T_FINAL22, temoin);
+                        Inverse22(T_FINAL22, temoin, T);
                         if(temoin == 0){
                             std::cout << "Cette matrice n'est pas inversible\n";
                         }
@@ -65,13 +78,15 @@ int main (int argc, char** argv) {
                         }
                     break;
                     case 6:
-                        Transposee22(T_FINAL22);
+                        lecture1(T);
+                        Transposee22(T_FINAL22, T);
                         std::cout << "La transposee est :\n";
                         std::cout << "( "  << T_FINAL22[0][0] << " " <<  T_FINAL22[0][1] << " )\n";
                         std::cout << "( "  << T_FINAL22[1][0] << " " <<  T_FINAL22[1][1] << " )\n";
                     break;
                     case 7:
-                        Trace22(trac);
+                        lecture1(T);
+                        Trace22(trac, T);
                         std::cout << "La trace est : " << trac << "\n";
                     break;
                     default:
@@ -91,33 +106,38 @@ int main (int argc, char** argv) {
                 std::cin >> choix2;
                 switch(choix2) {
                     case 1:
-                        Somme33(T_FINAL33);
+                        lecture2(T1_33, T2_33);
+                        Somme33(T_FINAL33, T1_33, T2_33);
                         std::cout << "La somme est  :\n";
                         std::cout << "( "  << T_FINAL33[0][0] << " " <<  T_FINAL33[0][1] << " " <<  T_FINAL33[0][2] << " )\n";
                         std::cout << "( "  << T_FINAL33[1][0] << " " <<  T_FINAL33[1][1] << " " <<  T_FINAL33[1][2] << " )\n";
                         std::cout << "( "  << T_FINAL33[2][0] << " " <<  T_FINAL33[2][1] << " " <<  T_FINAL33[2][2] << " )\n";
                     break;
                     case 2:
-                        Soustracion33(T_FINAL33);
+                        lecture2(T1_33, T2_33);
+                        Soustracion33(T_FINAL33, T1_33, T2_33);
                         std::cout << "La soustraction est :\n";
                         std::cout << "( "  << T_FINAL33[0][0] << " " <<  T_FINAL33[0][1] << " " <<  T_FINAL33[0][2] << " )\n";
                         std::cout << "( "  << T_FINAL33[1][0] << " " <<  T_FINAL33[1][1] << " " <<  T_FINAL33[1][2] << " )\n";
                         std::cout << "( "  << T_FINAL33[2][0] << " " <<  T_FINAL33[2][1] << " " <<  T_FINAL33[2][2] << " )\n";
                     break;
                     case 3:
-                        Multiplication33(T_FINAL33);
+                        lecture2(T1_33, T2_33);
+                        Multiplication33(T_FINAL33, T1_33, T2_33);
                         std::cout << "Le produit est :\n";
                         std::cout << "( "  << T_FINAL33[0][0] << " " <<  T_FINAL33[0][1] << " " <<  T_FINAL33[0][2] << " )\n";
                         std::cout << "( "  << T_FINAL33[1][0] << " " <<  T_FINAL33[1][1] << " " <<  T_FINAL33[1][2] << " )\n";
                         std::cout << "( "  << T_FINAL33[2][0] << " " <<  T_FINAL33[2][1] << " " <<  T_FINAL33[2][2] << " )\n";
                     break;
                     case 4:
-                        Determinant33(det);
+                        lecture1(T_33);
+                        Determinant33(det, T_33);
                         std::cout << "Le determinant est : " << det << "\n";
                     break;
                     case 5:
+                        lecture1(T_33);
                         temoin = 0;
-                        Inverse33(T_FINAL33, temoin);
+                        Inverse33(T_FINAL33, temoin, T_33);
                         if(temoin == 0){
                             std::cout << "Cette matrice n'est pas inversible\n";
                         }
@@ -129,14 +149,16 @@ int main (int argc, char** argv) {
                         }
                     break;
                     case 6:
-                        Transposee33(T_FINAL33);
+                        lecture1(T_33);
+                        Transposee33(T_FINAL33, T_33);
                         std::cout << "La transposee est :\n";
                         std::cout << "( "  << T_FINAL33[0][0] << " " <<  T_FINAL33[0][1] << " " <<  T_FINAL33[0][2] << " )\n";
                         std::cout << "( "  << T_FINAL33[1][0] << " " <<  T_FINAL33[1][1] << " " <<  T_FINAL33[1][2] << " )\n";
                         std::cout << "( "  << T_FINAL33[2][0] << " " <<  T_FINAL33[2][1] << " " <<  T_FINAL33[2][2] << " )\n";
                     break;
                     case 7:
-                        Trace33(trac);
+                        lecture1(T_33);
+                        Trace33(trac, T_33);
                         std::cout << "La trace est : " << trac << "\n";
                     break;
                     default:
