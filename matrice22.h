47116f75335e58319a9abd668d45910c19a00645
Martinmoudime <martinjuju025@gmail.com>
2024-10-27 16:58:36 +0100

    Update matrice22.h

--- matrice22.h
+++ matrice22.h
@@ -1,7 +1,14 @@
-void Somme22(float result[2][2]);
-void Soustracion22(float result[2][2]);
-void Multiplication22(float result[2][2]);
-void Determinant22(float &determinant);
-void Transposee22(float result[2][2]);
-void Inverse22(float result[2][2], int &t);
-void Trace22(float &trace);
+#ifndef __MATREICE22_H__
+#define __MATREICE22_H__  
+
+void Somme22(float result[2][2], float T1[2][2], float T2[2][2]);
+void Soustracion22(float result[2][2], float T1[2][2], float T2[2][2]);
+void Multiplication22(float result[2][2], float T1[2][2], float T2[2][2]);
+void Determinant22(float &determinant, float T[2][2]);
+void Transposee22(float result[2][2], float T[2][2]);
+void Inverse22(float result[2][2], int &t, float T[2][2]);
+void Trace22(float &trace, float T[2][2]);
+void lecture1(float T[2][2]);
+void lecture2(float T1[2][2], float T2[2][2]);
+
+#endif
